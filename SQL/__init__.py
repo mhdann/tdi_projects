122626359b9f93f41165f03d405a634a19ab276d
Michael Dann <mhdann@gmail.com>
2016-03-23 00:26:04 -0400

    SQL attempt 2

--- SQL/__init__.py
+++ SQL/__init__.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8-*-
 import fellow
 import typecheck
+import pandas as pd
 
 @fellow.app.task(name="sql.score_by_zipcode")
 @typecheck.returns("92 * (string, number, number, count)")
 def score_by_zipcode():
-    return [("11201", 9.81739130434783, 0.394278849322024, 345)] * 92
+    result = pd.read_csv("score_by_zipcode.csv")
+    return zip(result)
+    #return [("11201", 9.81739130434783, 0.394278849322024, 345)] * 92
 
 @fellow.app.task(name="sql.score_by_map")
 @typecheck.returns("string")
 def score_by_map():
     # must be url of the form https://x.cartodb.com/...
-    return "https://cartodb.com"
+    return "https://mhdann.cartodb.com/viz/b38e8ca4-f073-11e5-bb80-0e787de82d45/public_map"
 
 @fellow.app.task(name="sql.score_by_borough")
 @typecheck.returns("5 * (string, number, number, count)")
 def score_by_borough():
-    return [("MANHATTAN", 10.7269875502402, 0.0798259390597376, 10201)] * 5
+    data = pd.read_csv("score_boroughs.csv")
+    return(zip(data['borough'], data['mean'], data['std'], data['size']))
+# return [("MANHATTAN", 10.7269875502402, 0.0798259390597376, 10201)] * 5
 
 @fellow.app.task(name="sql.score_by_cuisine")
 @typecheck.returns("75 * (string, number, number, count)")
