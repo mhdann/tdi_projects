c9e377ab45e3a9ec5631fc99d68fcd747d1d3449
Michael Dann <mhdann@gmail.com>
2016-03-24 18:31:33 -0400

    SQL commit 20 something

--- SQL/__init__.py
+++ SQL/__init__.py
@@ -41,7 +41,12 @@ def score_by_cuisine():
 @fellow.app.task(name="sql.violation_by_cuisine")
 @typecheck.returns("20 * ((string, string), number, count)")
 def violation_by_cuisine():
-    return [(("Café/Coffee/Tea",
-              "Toilet facility not maintained and provided with toilet paper; "
-              "waste receptacle and self-closing door."),
-             1.87684775827172, 315)] * 20
+    import pandas as pd
+    import numpy as np
+    data = pd.read_csv("./SQL/violation_by_cuisine.csv", dtype = {"A": np.int64})
+    return zip(zip(data.CODEDESC, data.VIOLATIONDESC), data.ratio, data.A)
+#    return [(("Café/Coffee/Tea",
+#              "Toilet facility not maintained and provided with toilet paper; "
+#              "waste receptacle and self-closing door."),
+#             1.87684775827172, 315)] * 20
+
